1341995e6cef348acbdee5feb573169e65ca7830
Abdulrahman <Abdullrahmanmoez@gmail.com>
2024-12-06 22:43:51 +0200

    update

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save 
+from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 class Author(models.Model):
     name = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name
-    
+
 class Book(models.Model):
-    title = models.CharField(max_length= 100)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book')
     
     def __str__(self):
@@ -21,24 +22,19 @@ class Library(models.Model):
     
     def __str__(self):
         return self.name
-    
-    
+
+    class Meta:
+        permissions = [
+            ("can_add_books", "Can add books"),
+            ("can_remove_books", "Can remove books"),
+        ]
+
 class Librarian(models.Model):
     name = models.CharField(max_length=50)
-    library = models.OneToOneField(Library, on_delete= models.CASCADE)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
-
-
-
-
-
-
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
@@ -60,5 +56,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
-
-
