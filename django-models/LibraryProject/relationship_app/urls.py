cebf73d2ee28b7bc405926d4d0d63c230c2f0f64
Abdulrahman <Abdullrahmanmoez@gmail.com>
2024-12-06 22:18:03 +0200

    update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),  
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
-path('admin-dashboard/', views.admin_view, name='admin_dashboard'),
+    path('admin-dashboard/', views.admin_view, name='admin_dashboard'),
     path('librarian-panel/', views.librarian_view, name='librarian_panel'),
     path('member-dashboard/', views.member_view, name='member_dashboard'),
 ]
