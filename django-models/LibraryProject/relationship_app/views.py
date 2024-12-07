ab7b27ec58dbc8e5213c3da7476d8b7e8ed93d21
Abdulrahman <Abdullrahmanmoez@gmail.com>
2024-12-07 14:30:46 +0200

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,13 @@
-# from django.shortcuts import render
-# from django.contrib.auth.decorators import user_passes_test, permission_required
-# from django.views.generic.detail import DetailView
-# from .models import Library, Book, Author
-# from django.contrib.auth.forms import UserCreationForm
-# from django.urls import reverse_lazy
-# from django.views.generic import CreateView
-# from django.contrib.auth.views import LoginView, LogoutView
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.views.generic.detail import DetailView
+from .models import Library, Book, Author
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
 
-@login_required
+
 def list_books(request):
     books = Book.objects.all()
     authors = Author.objects.all()
@@ -28,7 +27,7 @@ class LibraryDetailView(DetailView):
         context['book_list'] = self.object.books.all()
         return context
 
-class RegisterView(CreateView):
+class Register(CreateView):
     form_class = UserCreationForm
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
@@ -37,34 +36,34 @@ class RegisterView(CreateView):
         response = super().form_valid(form)
         return response
 
-class CustomLoginView(LoginView):
+class LoginView(LoginView):
     template_name = 'relationship_app/login.html'
     redirect_authenticated_user = True
 
-class CustomLogoutView(LogoutView):
+class LogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
     next_page = reverse_lazy('login')
 
-def user_is_admin(user):
+def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-def user_is_librarian(user):
+def is_librarian(user):
     return user.userprofile.role == 'Librarian'
 
-def user_is_member(user):
+def is_member(user):
     return user.userprofile.role == 'Member'
 
-@user_passes_test(user_is_admin)
+@user_passes_test(is_admin)
 @permission_required('Library.can_add_books', raise_exception=True)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(user_is_librarian)
+@user_passes_test(is_librarian)
 @permission_required('Library.can_change_book', raise_exception=True)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
-@user_passes_test(user_is_member)
+@user_passes_test(is_member)
 @permission_required('Library.can_delete_book', raise_exception=True)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
